2b0639fb017cee0c545f271eee3f1089a3b5e783
Pedro Polez <pedrorocha@gec.inatel.br>
2017-10-07 17:11:24 -0300

    Implementado "BD" de músicas
    - Leitura de arquivo de músicas
    - Geração de lista global estática com todas as músicas
    - Geração da lista com 4 músicas somente para votação

--- beermusic/beermusic/Music.cs
+++ beermusic/beermusic/Music.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 class Music
 {
-    public int id { get; set; }
     public string name { get; set; }
-    public int duration { get; set; }
+    public string artist { get; set; }
+    public string url { get; set; }
+
+    public static List<Music> musicDB = new List<Music>();
+    public static bool hasDBPopulated { get; set; } = false;
+
+    Music(string songName, string artistName, string spotifyUrl)
+    {
+        this.name = songName;
+        this.url = spotifyUrl;
+        this.artist = artistName;
+    }
+
+    public static void parseMusicList()
+    {
+        string line;
+        //Cria o leitor de arquivos e lê os dados.
+        System.IO.StreamReader fileReader = new System.IO.StreamReader("musicDb.txt");
+        while ((line = fileReader.ReadLine()) != null)
+        {
+            Debug.WriteLine(line);
+            var splitResult = line.Split('|');
+            Music newMusic = new Music(splitResult[0], splitResult[1], splitResult[2]);
+            musicDB.Add(newMusic);
+        }
+        hasDBPopulated = true;
+        Debug.WriteLine("Music database parsing complete.");
+    }
+
+
 }
