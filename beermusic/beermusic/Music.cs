fa93f0b0e6ea0380b9fd1338ffd457d6ba3a30f1
Eduardo Augusto de Oliveira Pereira <eduardoaugusto93@gmail.com>
2017-10-14 20:22:13 -0300

    ajustando html e javascript para receber listas de musicas e enviar musica selecionada

--- beermusic/beermusic/Music.cs
+++ beermusic/beermusic/Music.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 class Music
 {
@@ -12,6 +13,7 @@ class Music
     public string url { get; set; }
 
     public static List<Music> musicDB = new List<Music>();
+    public static Dictionary<string, Music> musicsMap = new Dictionary<string, Music>();
     public static bool hasDBPopulated { get; set; } = false;
 
     Music(string songName, string artistName, string spotifyUrl)
@@ -37,5 +39,56 @@ class Music
         Debug.WriteLine("Music database parsing complete.");
     }
 
+    public static void parseMusicMap()
+    {
+        string line;
+        //Cria o leitor de arquivos e lê os dados.
+        System.IO.StreamReader fileReader = new System.IO.StreamReader("musicDb.txt");
+        while ((line = fileReader.ReadLine()) != null)
+        {
+            Debug.WriteLine(line);
+            var splitResult = line.Split('|');
+            Music newMusic = new Music(splitResult[0], splitResult[1], splitResult[2]);
+            string[] stringSeparators = new string[] { "track/" };
+            var splitUrl = splitResult[2].Split(stringSeparators, StringSplitOptions.None);
+            musicsMap.Add(splitUrl[1], newMusic);
+        }
+        hasDBPopulated = true;
+        Debug.WriteLine("Music database parsing complete.");
+    }
 
+    public static string getJsonFromList()
+    {
+        List<Music> someMusics = new List<Music>();
+        Random r = new Random();
+        int lastRandom = r.Next(0, musicDB.Count); //for ints
+        for (int i = 0; i < 4; i++)
+        {
+            lastRandom = (lastRandom * (i + 1) * 19) % musicDB.Count;
+            int j = 1;
+            while(checkForRepeatedMusics(musicDB[lastRandom], someMusics))
+            {
+                lastRandom = (lastRandom * (i + j) * 19) % musicDB.Count;
+                j++;
+            }
+            someMusics.Add(musicDB[lastRandom]);
+        }
+        
+        var json = JsonConvert.SerializeObject(someMusics);
+        return json;
+    }
+
+    //return true case has some equal music and false case no one equal
+    public static bool checkForRepeatedMusics(Music music, List<Music> musics)
+    {
+        for (int i = 0; i < musics.Count; i++)
+        {
+            if (musics[i].artist.Equals(music.artist) 
+                || musics[i].name.Equals(music.name))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
